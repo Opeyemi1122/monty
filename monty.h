5aaacfe818debb1e856783907de239cd08eadce2
Opeyemi1122 <alabiopex@gmail.com>
2024-01-17 22:55:39 +0100

    wrong push

--- monty.h
+++ monty.h
@@ -37,6 +37,7 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void f_push(stack_t **head, unsigned int counter)
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 
